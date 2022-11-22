04953fb1c99aa6e5f8769ca39d1e15352bfe6734
twosky0202 <gahaneul0202@gmail.com>
2022-11-22 16:39:24 +0900

    숙소가 필요해 퍼블리싱

---
+++ app/src/main/java/com/globalexchange/app/controller/NeedLodgingController.java
@@ -0,0 +1,22 @@
+package com.globalexchange.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/needLodging/*")
+public class NeedLodgingController {
+
+    // 숙소가 필요해 상세 페이지
+    @GetMapping("/detail")
+    public void detail(){
+
+    }
+
+    // 숙소가 필요해 목록 페이지
+    @GetMapping("/list")
+    public void list(){
+
+    }
+}
