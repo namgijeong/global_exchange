853bb927a137edd1cfac2d5ad7491f8834b591ac
han-kyuhyun <kyuhyun9902@gmail.com>
2022-11-23 16:12:55 +0900

    폴더 추가

---
+++ app/src/main/java/com/globalexchange/app/service/BoardService.java
@@ -0,0 +1,24 @@
+//package com.globalexchange.app.service;
+//
+//import com.example.app.domain.vo.BoardDTO;
+//import com.example.app.domain.vo.BoardVO;
+//import com.example.app.domain.vo.Criteria;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public interface BoardService {
+//    //    추가
+//    public void register(BoardDTO boardDTO);
+//    //    수정
+//    public void modify(BoardDTO boardDTO);
+//    //    삭제
+//    public void remove(Long boardNumber);
+//    //    조회
+//    public BoardDTO show(Long boardNumber);
+//    //    전체 조회
+//    public List<BoardVO> showAll(Criteria criteria);
+////    전체 개수
+//    public int getTotal();
+//}
