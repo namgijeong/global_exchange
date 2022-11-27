3bbc5aa90ea0f18c420f9dcf43f5017e88061f4e
han-kyuhyun <kyuhyun9902@gmail.com>
2022-11-27 14:39:55 +0900

    로그인 , 회원가입 , 중복검사 작업

--- app/src/main/java/com/globalexchange/app/mapper/MemberMapper.java
+++ app/src/main/java/com/globalexchange/app/mapper/MemberMapper.java
@@ -1,10 +1,15 @@
 package com.globalexchange.app.mapper;
 
+import com.globalexchange.app.domain.vo.MemberVO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
-    public int checkId(String memberId, String memberPwd);
+   int checkId(String checkId);
+   int join(MemberVO memberVO);
+   MemberVO login(MemberVO memberVO);
 
+//   public void MemberJoin(MemberVO memberVO);
 
 }
--- app/src/main/resources/static/js/member/login.js
+++ app/src/main/resources/static/js/member/login.js
@@ -32,3 +32,27 @@ function onSignIn(googleUser) {
 function onSignInFailure(t){
     console.log(t);
 }
+
+function checkValue()
+{
+    if(!document.userInfo.id.value){
+        alert("아이디를 입력하세요.");
+        return false;
+    }
+
+    if(!document.userInfo.password.value){
+        alert("비밀번호를 입력하세요.");
+        return false;
+    }
+
+    // 비밀번호와 비밀번호 확인에 입력된 값이 동일한지 확인
+    if(document.userInfo.password.value != document.userInfo.passwordcheck.value ){
+        alert("비밀번호를 동일하게 입력하세요.");
+        return false;
+    }
+}
+
+// 취소 버튼 클릭시 로그인 화면으로 이동
+function goLoginForm() {
+    location.href="LoginForm.jsp";
+}
