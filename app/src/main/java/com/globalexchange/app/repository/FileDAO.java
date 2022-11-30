f473850bc7e18af9b0c86ea2fc63fdd384316883
namgijeong <dreaming1817@gmail.com>
2022-11-30 16:46:54 +0900

    만남과 도움 요청글 crud 구현(로그인이후 실험은 못함, 삭제와 수정버튼 보였다안보이게 하는거 아직 구현못함)

--- app/src/main/java/com/globalexchange/app/repository/FileDAO.java
+++ app/src/main/java/com/globalexchange/app/repository/FileDAO.java
@@ -1,6 +1,7 @@
 package com.globalexchange.app.repository;
 
 
+import com.globalexchange.app.domain.vo.FileMeetVO;
 import com.globalexchange.app.domain.vo.FileProfileVO;
 import com.globalexchange.app.mapper.FileMapper;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +22,21 @@ public class FileDAO {
 //    public void remove(Long boardNumber){
 //        fileMapper.delete(boardNumber);
 //    }
-    //    파일 조회
+    //    프로필이미지 조회
     public FileProfileVO getMeetWriterImage(Long memberNumber){
         return fileMapper.getMeetWriterImage(memberNumber);
+    }
+    // meet게시글 이미지 등록
+    public void insertMeetFile(FileMeetVO fileMeetVO){
+        fileMapper.insertMeetFile(fileMeetVO);
+    }
+
+    //meet 게시글 파일 불러오기
+    public FileMeetVO getMeetFile(Long meet_number){
+        return fileMapper.getMeetFile(meet_number);
+    }
+    //meet 게시글 수정할때 meet게시글에 원래 있던 파일 지우기
+    public void deleteMeetFile(Long meet_number){
+        fileMapper.deleteMeetFile(meet_number);
     }
 }
