112bf2afc6e6106c040aba6f0680d07926206aad
han-kyuhyun <kyuhyun9902@gmail.com>
2022-12-12 16:16:08 +0900

    숙소가 필요해 리스트 뽑기 완료

--- app/src/main/java/com/globalexchange/app/repository/LodgingDAO.java
+++ app/src/main/java/com/globalexchange/app/repository/LodgingDAO.java
@@ -17,6 +17,29 @@ import java.util.List;
 public class LodgingDAO {
     private final LodgingMapper lodgingMapper;
 
+    // 숙소가 필요해 리스트
+    public List<LodgingVO> lodgingSelectAll(Criteria criteria){
+        return lodgingMapper.lodgingSelectAll(criteria);
+    }
+
+    // 리스트 전체 갯수
+
+    public int getTotal(){
+        return lodgingMapper.getTotal();
+    }
+
+    // 리스트 카테고리
+    public List<LodgingVO> categoryLodgingSelectAll(Criteria criteria,String lodgingLearningLang){
+        return lodgingMapper.categoryLodgingSelectAll(criteria,lodgingLearningLang);
+    }
+
+
+    // 카테고리 게시글 전체 갯수
+
+    public int categoryGetTotal(String lodgingLearningLang){
+        return lodgingMapper.categoryGetTotal(lodgingLearningLang);
+    }
+
     // 답글이 없는 최신글 조회
     public List<LodgingVO> findAllLatestNotAnsweredLodging(Criteria criteria){
         return lodgingMapper.selectAllLatestNotAnsweredLodging(criteria);
