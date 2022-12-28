93094730f2b9016ad43d5b69c92659df11dc926b
twosky0202 <gahaneul0202@gmail.com>
2022-12-28 17:18:18 +0900

    내가 작성한 일기, 파트너가 작성한 일기 페이지 완료

--- app/src/main/java/com/globalexchange/app/service/DiaryService.java
+++ app/src/main/java/com/globalexchange/app/service/DiaryService.java
@@ -16,10 +16,33 @@ public interface DiaryService {
     public void remove(Long diaryNumber);
     //    조회
     public DiaryDTO show(Long diaryNumber);
+    //    파트너가 나에게 일기 썼을 때 조회
+    public DiaryDTO showWrittenByPartner(Long diaryNumber);
     //    전체 조회
     public List<DiaryDTO> showAll(Criteria criteria);
+
+    //  내가 쓴 일기 전체 조회
+    public List<DiaryDTO> myDiarySelectAll(Criteria criteria, Long memberNumber);
+    // 나와 특정 파트너의 일기 내용
+    public List<DiaryDTO> myPartnerSelectAll(Long memberNumber, Long diaryPartnerNumber, Criteria criteria);
+
+    //  파트너가 나에게 쓴 일기 전체 조회
+    public List<DiaryDTO> toMeDiarySelectAll(Criteria criteria, Long memberNumber);
+    //  특정 파트너가 나에게 쓴 일기 전체 조회
+    public List<DiaryDTO> toMeFromPartnerDiarySelectAll(Criteria criteria, Long memberNumber, Long diaryPartnerNumber);
+
     //    전체 개수
     public int getTotal();
+
+    //  내가 쓴 일기 전체 개수
+    public int myDiaryGetTotal(Long memberNumber);
+    // 나와 특정 파트너의 일기 수
+    public int categoryGetTotal(Long memberNumber, Long diaryPartnerNumber);
+
+    //  파트너가 나에게 쓴 일기 전체 개수
+    public int toMeDiaryGetTotal(Long memberNumber);
+    //  특정 파트너가 나에게 쓴 일기 전체 개수
+    public int toMeFromPartnerDiaryGetTotal(Long memberNumber, Long diaryPartnerNumber);
 
     // 파트너 리스트 조회
     public List<MemberDTO> showAllPartner(Criteria criteria);
@@ -41,6 +64,8 @@ public interface DiaryService {
 
     // 파트너 신고
     public void registerReport(Long reportingMemberNumber, Long reportedMemberNumber);
+
+
 }
 
 
